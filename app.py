805525070fdf2481678fa55bb0b84b18509a1bc0
msochel <msochel@unal.edu.co>
2017-06-26 12:10:43 -0500

    run one file in app.py

--- app.py
+++ app.py
@@ -21,47 +21,40 @@ app.static("/static", "./static")
 @app.route("/", methods=["POST", "GET"])
 async def start(request):
     if request.method == "POST":
-        req_file = request.files.getlist("file")
-        for file_ in req_file:
-            file_name = file_.name
-            print(file_name)
-            body = file_.body.decode("unicode_escape")
-            with open("files/" + file_name, "w") as file:
-                file.write(body)
-            file_path = "files/" + file_name
-            graph = ig.read(file_path, format="graphml")
-            colors = {
-                        "#3217A7":10008,
-                        "#F0F007": 205005,
-                        "White":163001,
-                        "#9725A4":135007,
-                        "#000000": 164010,
-                        "#156911":189010,
-                        '#A71717':157000,
-                        "#2E2E2E": 164009,
-                    }
-            for pos, x in enumerate(graph.vs['color']):
-                graph.vs[pos]['color'] = colors[x]
+        req_file = request.files.get("file")
+        file_name = req_file.name
+        body = req_file.body.decode("unicode_escape")
+        with open("files/" + file_name, "w") as file:
+            file.write(body)
+        file_path = "files/" + file_name
+        graph = ig.read(file_path, format="graphml")
+        colors = {
+                    "#3217A7":10008,
+                    "#F0F007": 205005,
+                    "White":163001,
+                    "#9725A4":135007,
+                    "#000000": 164010,
+                    "#156911":189010,
+                    '#A71717':157000,
+                    "#2E2E2E": 164009,
+                }
+        for pos, x in enumerate(graph.vs['color']):
+            graph.vs[pos]['color'] = colors[x]
 
-            for pos, x in enumerate(graph.es['color']):
-                graph.es[pos]['color'] = colors[x]
+        for pos, x in enumerate(graph.es['color']):
+            graph.es[pos]['color'] = colors[x]
 
-            url = graphistry.bind(
-                        source='src',
-                        destination='dst',
-                        point_color='color',
-                        edge_color='color'
-                    ).plot(graph)
-            # print(type(url))
-            # view = env.get_template("home.html")
-            # html_content = view.render(url=url)
-            # hola.append(html_content)
+        url = graphistry.bind(
+                    source='src',
+                    destination='dst',
+                    point_color='color',
+                    edge_color='color'
+                ).plot(graph)
+        # print(type(url))
 
         view = env.get_template("home.html")
-        html_content = view.render(url="null")
+        html_content = view.render(url=url)
         return html(html_content)
-        # hola.append(view.render(url="null"))
-        # return html(hola[0], hola[1], hola[2])
     elif request.method == "GET":
         template = env.get_template("home.html")
         html_content = template.render(url="null")
